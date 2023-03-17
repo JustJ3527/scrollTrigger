8fa61a81ff2b99c1bb74d800e28bcbdcb355401c
JustJ3527 <antoine.jules04@gmail.com>
2023-03-17 22:30:15 +0100

    maj

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,39 @@
 gsap.registerPlugin(ScrollTrigger);
 
+var timeOut;
+const panel1 = document.getElementById("panel1"),
+    panel2 = document.getElementById("panel2");
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function onLoad() {
+    timeOut = setTimeout(showPage, 800);
+    
+}
+
+function showPage() {
+    panel1.style.transform = "scale(1, -0.2)";
+    sleep(200).then(() => {
+        panel2.style.transform = "scale(1, 0)";
+    });
+}
+
 //Redicrection
 const panel = document.getElementById("panel");
 function redirection() {
-    panel.style.transform = "none"; 
+    panel2.style.transform = "none";
+    sleep(200).then(() => {
+        panel1.style.transform = "none";
+    });
 }
 
-let mybutton = document.getElementById("navbar");
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 150) {
-    mybutton.style.backgroundColor = "var(--darkColor)";
-} else {
-    mybutton.style.backgroundColor = "transparent";
-}
-}
 
 const body = document.body,
     scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
     height = scrollWrap.getBoundingClientRect().height - 1,
-    speed = 0.1;
+    speed = 0.08;
 
 var offset = 0;
 
@@ -38,38 +52,34 @@ smoothScroll();
 
 var markersValue = "false";
 
-gsap.to(".panel", {
-	scaleY: 0,
-	duration: 1.65,
-	ease: "power4.inOut"
+
+gsap.set(".letter", {
+        scale: 1.15,
 });
 
-const nameTL = gsap.timeline();
-
-nameTL
-    .set(".letter", {
-        scale: 1.4,
-    })
-    .from(".letter", {
+gsap.from(".letter", {
         yPercent: 150,
         opacity: 0,
         stagger: 0.018,
 		duration: 1.6,
 		ease: "power4.inOut"
-    }, "+=0.85")
+    }, "+=0.85");
 
-    .to(".letter", {
+gsap.to(".letter", {
         scale: 1,
         duration: 0.95,
         ease: "power3.out"
-    },"-=0.75")
+    },"-=0.1");
 
-    .from(".container-img", { 
+
+gsap.from(".container-img", { 
         opacity: 0, 
         duration: 5, 
-        ease: "power2.out" 
-    }, "-=0.85");
+        ease: "none" 
+});
 
+
+//Parallax
 gsap.to(".container-text", {
     yPercent: -20,
     ease: "none",
@@ -79,14 +89,14 @@ gsap.to(".container-text", {
         // end: "bottom top",
         scrub: true
     }, 
-    });
-    gsap.to(".container-img", {
-        yPercent: 30,
-        ease: "none",
-        scrollTrigger: {
-            trigger: ".container-img",
-            start: "center 50%", // the default values
-            end: "center top",
-            scrub: true
-        }, 
-      });
+});
+gsap.to(".container-img", {
+    yPercent: 30,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".container-img",
+        start: "center 50%", // the default values
+        end: "center top",
+        scrub: true
+    }, 
+});
