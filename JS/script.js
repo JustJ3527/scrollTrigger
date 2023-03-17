7b73c986a91321a0c5dd9c3181b97c9ccc922f79
JustJ3527 <antoine.jules04@gmail.com>
2023-03-17 22:51:03 +0100

    maj2

--- JS/script.js
+++ JS/script.js
@@ -30,25 +30,24 @@ function redirection() {
 }
 
 
-const body = document.body,
-    scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
-    height = scrollWrap.getBoundingClientRect().height - 1,
-    speed = 0.08;
+// const body = document.body,
+//     scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
+//     height = scrollWrap.getBoundingClientRect().height - 1,
+//     speed = 0.08;
 
-var offset = 0;
+// var offset = 0;
 
-body.style.height = Math.floor(height) + "px";
 
-function smoothScroll() {
-    offset += (window.pageYOffset - offset) * speed;
+// function smoothScroll() {
+//     offset += (window.pageYOffset - offset) * speed;
 
-    var scroll = "translateY(-" + offset + "px) translateZ(0)";
-    scrollWrap.style.transform = scroll;
+//     var scroll = "translateY(-" + offset + "px) translateZ(0)";
+//     scrollWrap.style.transform = scroll;
 
-    callScroll = requestAnimationFrame(smoothScroll);
-}
+//     callScroll = requestAnimationFrame(smoothScroll);
+// }
 
-smoothScroll();
+// smoothScroll();
 
 var markersValue = "false";
 
